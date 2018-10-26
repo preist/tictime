83a805a21a8957e7f7b824d47d5bbd84482689e6
Igor Putina <iputina@mixbook.com>
2018-10-26 22:47:32 +0300

    Add categories placeholder

---
+++ src/components/category.tsx
@@ -0,0 +1,42 @@
+export const CATEGORIES: object = {
+  1: {
+    color: '#F5A623',
+    title: 'Work',
+  },
+  2: {
+    color: '#4A90E2',
+    title: 'Meetings',
+  },
+  3: {
+    color: '#50E3C2',
+    title: 'Transit',
+  },
+  4: {
+    color: '#7ED321',
+    title: 'Family',
+  },
+  5: {
+    color: '#1EABAA',
+    title: 'Self Development',
+  },
+  6: {
+    color: '#0094FF',
+    title: 'Play',
+  },
+  7: {
+    color: '#F5A623',
+    title: 'Spiritual',
+  },
+  8: {
+    color: '#4A90E2',
+    title: 'Exercise',
+  },
+  9: {
+    color: '#9E3380',
+    title: 'Food',
+  },
+  10: {
+    color: '#A7AA99',
+    title: 'Sleep',
+  }
+}
--- src/components/log_entry.tsx
+++ src/components/log_entry.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 
 import { differenceInMinutes, format } from 'date-fns';
 
+import { CATEGORIES } from 'components/category';
+
 export interface ILogEntryProps {
   title: string;
   category: number;
@@ -19,11 +21,12 @@ export default class LogEntry extends React.Component<ILogEntryProps> {
 
   public render() {
     const {title, category, startAt, endAt} = this.props;
+    const logCategory = CATEGORIES[category];
 
     return (
       <div className="log-entry">
         <span className="log-entry--title">{title}</span>
-        <span className="log-entry--category">{category}</span>
+        <span className="log-entry--category" color={logCategory.color}>{logCategory.title}</span>
         <span className="log-entry--start-at">{format(startAt, "hh:mm a")}</span>
         <span className="log-entry--end-at">{format(endAt, "hh:mm a")}</span>
         <span className="log-entry--duration">{differenceInMinutes(endAt, startAt)} minutes</span>
