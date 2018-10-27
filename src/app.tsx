666a1e73f496989d83c21e7e0005ac2ae3e88780
Igor Putina <iputina@mixbook.com>
2018-10-27 14:44:05 +0300

    Fix styles

--- src/app.tsx
+++ src/app.tsx
@@ -15,18 +15,20 @@ class App extends React.Component {
         <p className="app-description">
           A time tracking tool
         </p>
-        <LogEntry
-          title="Made some coffee and started work"
-          category={1}
-          startAt={subHours(new Date(), 1)}
-          endAt={new Date(new Date())}
-        />
-        <LogEntry
-          title="Took subway work"
-          category={3}
-          startAt={subHours(new Date(), 2)}
-          endAt={subHours(new Date(), 1)}
-        />
+        <div className="log-entries">
+          <LogEntry
+            title="Made some coffee and started work attempting a longer title"
+            category={1}
+            startAt={subHours(new Date(), 1)}
+            endAt={new Date(new Date())}
+          />
+          <LogEntry
+            title="Took subway work"
+            category={3}
+            startAt={subHours(new Date(), 2)}
+            endAt={subHours(new Date(), 1)}
+          />
+        </div>
       </div>
     );
   }
